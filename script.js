77d3bff22dd8e6984982f46e9b29c9d9a251c610
Lamin Sowe <Lamin2.Sowe@live.uwe.ac.uk>
2024-02-23 15:23:16 +0000

    mapping

---
+++ script.js
@@ -0,0 +1,18 @@
+function getLocation(page) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function(loc) { // permission granted
+            location.href=`${page}?lat=${loc.coords.latitude}&lon=${loc.coords.longitude}`;
+          },
+          function() { // permission denied
+            // location defaults to central Bristol
+            location.href=`${page}?lat=51.454514&lon=-2.587910`;              
+          }
+        )
+    }
+    else { // unsupported feature
+        alert("Geolocation is not supported by this browser.");
+        // location defaults to central Bristol
+        location.href=`${page}?lat=51.454514&lon=-2.587910`;
+    }
+}
